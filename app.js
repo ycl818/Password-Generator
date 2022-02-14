0d1bfffa9b890fcbd8d1d0d8c644c03940176d0d
ycl818 <kevinyichienlee@gmail.com>
2022-02-14 18:46:38 -0500

    ADD: setting body-parser

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const exphbs = require("express-handlebars");
 const app = express();
 const port = 3000;
 
-//set template engine
+//setting template engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// setting body-parser
+app.use(express.urlencoded({ extended: true }));
+
+// setting routes
 app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/", (req, res) => {
+  console.log("req.body", req.body);
   res.render("index");
 });
 
