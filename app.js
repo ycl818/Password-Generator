f892bd100677f0841f595ab4164b5adb16362f6a
ycl818 <kevinyichienlee@gmail.com>
2022-02-14 00:31:09 -0500

    ADD: views engine and handlebars template files

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+//set template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 app.get("/", (req, res) => {
-  res.send("This will be a random password generator");
+  res.render("index");
 });
 
 app.listen(port, () => {
